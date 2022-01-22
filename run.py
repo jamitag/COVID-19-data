0044ed15be7a5328b50ff847e57dce9f5cdb83cf
jamitag <jamgra12@gmail.com>
2022-01-22 15:21:17 +0000

    Add diet class and test search

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import requests
 import simpleSearch
 url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=c36e1425cde0460bb5ee0bec9952d123"
 c = simpleSearch.Cuisine(url)
-print(d.search("italian"))
-d.printUrl()
-
+d = simpleSearch.Diet(url)
+print(d.search("french"))
+c.printUrl()
 
--- simpleSearch.py
+++ simpleSearch.py
@@ -4,3 +4,7 @@ import baseClasses as bc
 class Cuisine(bc.Category):
     def __init__(self, api):
         bc.Category.__init__(self, api, "cuisine") #initialises the api to search by cuisine
+
+class Diet(bc.Category):
+    def __init__(self, api):
+        bc.Category.__init__(self, api, "diet") #initialises the api to search by diet
